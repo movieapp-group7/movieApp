2a4e39c8141919af0f781e2bcd6d698b8ca24a8f
rahul-kalwar <rahulkumarkalwar40@gmail.com>
2024-12-01 13:59:41 +0200

    footer added with logo

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
@@ -10,6 +11,7 @@ const MainLayout = () => (
     <main>
       <Outlet /> {/* This will render the child routes */}
     </main>
+    <Footer /> {/* Add Footer here */}
   </div>
 );
 
